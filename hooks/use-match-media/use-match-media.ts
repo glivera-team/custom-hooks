bfdcb2d0f255b6e11b6bae38ee8aace61777d8eb
Yevhen Dudiak <e.dudyak@gmail.com>
2023-07-24 12:10:45 +0300

    refactor: update useMatchMedia hook, add types for return value & format code

--- hooks/use-match-media/use-match-media.ts
+++ hooks/use-match-media/use-match-media.ts
@@ -1,36 +1,39 @@
-import { useState, useLayoutEffect } from "react";
+import { useState, useEffect } from "react";
+
+interface MatchMediaValues {
+	isMobile: boolean;
+	isDesktop: boolean;
+	isTablet: boolean;
+}
 
 const QUERIES: Array<string> = [
 	"(max-width: 1023px)",
-	"(min-width: 1024px)",
-	"(max-width: 767px)",
+	"(min-width: 1024px) and (max-width: 1365px)",
+	"(min-width: 1366px)",
 ];
 
-export const useMatchMedia = (): {
-	[key: string]: boolean,
-} => {
-	const mediaQueryLists: MediaQueryList[] = typeof window !== 'undefined' ? QUERIES.map((query) => window?.matchMedia(query)) : [];
+const useMatchMedia = (): MatchMediaValues => {
+	const mediaQueryLists: MediaQueryList[] = typeof window !== "undefined" ? QUERIES.map((query) => window?.matchMedia(query)) : [];
 
 	const getValues = () => mediaQueryLists.map((list) => list.matches);
 
 	const [values, setValues] = useState(getValues);
 
-	useLayoutEffect(() => {
+	useEffect(() => {
 		const handler = () => setValues(getValues);
 
 		mediaQueryLists.forEach((list) => list.addEventListener("change", handler));
 
-		return () =>
-			mediaQueryLists.forEach((list) =>
-				list.removeEventListener("change", handler)
-			);
+		return () => {
+			mediaQueryLists.forEach((list) => list.removeEventListener("change", handler));
+		};
 	}, []);
 
-	return ["isMobile", "isDesktop", "isTablet"].reduce(
-		(acc, screen, index) => ({
-			...acc,
-			[screen]: values[index]
-		}),
-		{}
-	);
+	return {
+		isMobile: values[0],
+		isDesktop: values[1],
+		isTablet: values[2],
+	};
 };
+
+export default useMatchMedia;
